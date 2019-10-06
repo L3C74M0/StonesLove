4d52c75cb25b8c3b05dbec9fcc33fb6806d62a5f
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2019-10-06 09:26:22 -0500

    Resuelto

--- src/model/StonesLove.java
+++ src/model/StonesLove.java
@@ -5,13 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class StonesLove {
-	static Node iq;
+	static int suma;
+	static int[] output;
 
 	public static void main(String[] args) throws IOException {
-
-		// Pos 0 = (dia = 1)
-		// pos 1 = sumatoria
-		iq = new Node(0, 0);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String line = br.readLine();
@@ -26,56 +23,68 @@ public class StonesLove {
 		line = br.readLine();
 		String[] consultas = line.split(" ");
 
-		Node[] array = new Node[amountDays];
+		int[] array = new int[amountDays];
 
 		for (int i = 0; i < array.length; i++) {
-			array[i] = new Node(1, Integer.parseInt(stones[i]));
+			array[i] = Integer.parseInt(stones[i]);
 		}
 
-		for (int j = 0; j < amountQueries; j++) {
-			System.out.println(cuantosDiasTardo(array, Integer.parseInt(consultas[j])).day);
+		suma = 0;
+		output = new int[amountDays];
+
+		cuantosDiasTardo(array);
+
+		for (int i = 0; i < amountQueries; i++) {
+			System.out.println(searchDays(output,Integer.parseInt(consultas[i])));
 		}
 	}
 
-	public static Node cuantosDiasTardo(Node[] array, int toSearch) {
-		return cuantosDiasTardo(array, 0, array.length - 1, toSearch);
+	public static void cuantosDiasTardo(int[] array) {
+		cuantosDiasTardo(array, 0, array.length - 1);
 	}
 
-	private static Node cuantosDiasTardo(Node[] array, int i, int j, int toSearch) {
-		Node izq;
-		Node der;
+	private static void cuantosDiasTardo(int[] array, int i, int j) {
 		int m = (i + j) / 2;
 
 		if (i == j) {
-			return array[i];
+			suma += array[i];
+			output[i] = suma;
 		} else {
-			izq = cuantosDiasTardo(array, i, m, toSearch);
+			cuantosDiasTardo(array, i, m);
 
-			if (izq.suma >= toSearch) {
-				Node temp = new Node(izq.day, izq.suma);
-				return temp;
-			}
-
-			der = cuantosDiasTardo(array, m + 1, j, toSearch);
-
-			if (izq.suma + der.suma >= toSearch) {
-				Node temp = new Node(izq.day + der.day, izq.suma + der.suma);
-				return temp;
-			}
-
-			Node temp = new Node(izq.day + der.day, izq.suma + der.suma);
-			iq = temp;
-			return temp;
+			cuantosDiasTardo(array, m + 1, j);
 		}
 	}
-}
 
-class Node {
-	public int day;
-	public int suma;
+	public static int searchDays(int[] vector, int toSearch) {
+		return searchDays(vector, 0, vector.length - 1, toSearch);
+	}
 
-	public Node(int day, int suma) {
-		this.day = day;
-		this.suma = suma;
+	private static int searchDays(int[] vector, int i, int j, int toSearch) {
+		int izq;
+		int der;
+		int m = (i + j) / 2;
+
+		if (i == j) {
+			return i;
+		} else {
+			if (vector[m] == toSearch) {
+				return m + 1;
+			} else {
+				if (vector[m] < toSearch) {
+					if (toSearch < vector[m + 1]) {
+						return m + 2;
+					} else {
+						return der = searchDays(vector, m + 1, j, toSearch);
+					}
+				} else {
+					if (toSearch > vector[m - 1]) {
+						return m + 1;
+					} else {
+						return izq = searchDays(vector, i, m, toSearch);
+					}
+				}
+			}
+		}
 	}
 }
